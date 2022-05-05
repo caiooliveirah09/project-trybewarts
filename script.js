b13e0fdd10fc8bcc3f01a76812e5af45caa72909
Emerson Florencio <emersonflorencio@hotmail.com>
2022-05-05 17:05:19 -0300

    Resolução dos requisitos 4 e 5

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function validateForm() {
   let email = document.forms["login"]["email"].value;
-  let senha = document.forms["login"]["senha"].value;
+  let senha = document.forms["login"]["password"].value;
   if (email == "tryber@teste.com" && senha == "123456") {
     alert("Olá, Tryber!")
   } else {
-    alert("Email ou senha inválidos")
+    alert("Email ou senha inválidos.")
   }
 }
 
+let button = document.getElementById("botao");
 
+button.addEventListener('click', validateForm)
